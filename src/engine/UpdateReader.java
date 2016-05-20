442489a98962d78856fa59b592e9046c31e14176
jolteon <443972553@qq.com>
2016-05-20 16:08:11 +0800

    f

--- src/engine/UpdateReader.java
+++ src/engine/UpdateReader.java
@@ -2,7 +2,6 @@ package engine;
 
 import macros.Macros;
 
-import javax.crypto.Mac;
 import java.io.RandomAccessFile;
 
 /**
@@ -27,50 +26,54 @@ public class UpdateReader {
         raw = new byte[Macros.buffer_size >> 1];
         result = new byte[Macros.buffer_size >> 1];
     }
+
     public int get(byte[] dst, int off, int len) throws Exception {
-        while (size < len) {
-            int result_size;
-            if (Macros.compressor != null) {
-                boolean has_value = uFiles[top].read(temp) > 0;
-                while (!has_value && top < uFiles.length) {
-                    ++top;
-                    if (top < uFiles.length) {
-                        has_value = uFiles[top].read(temp) > 0;
+        synchronized (uFiles) {
+            while (size < len) {
+                int result_size;
+                if (Macros.compressor != null) {
+                    boolean has_value = uFiles[top].read(temp) > 0;
+                    while (!has_value && top < uFiles.length) {
+                        ++top;
+                        if (top < uFiles.length) {
+                            has_value = uFiles[top].read(temp) > 0;
+                        }
                     }
-                }
-                if (top >= uFiles.length) {
-                    return -1;
-                }
-                int size = Macros.decodeInt(temp, 0);
-                System.out.println(raw.length + " " + size);
-                int cur = 0;
-                do {
-                    cur += uFiles[top].read(raw, cur, size - cur);
-                } while (cur < size);
-                result_size = Macros.compressor.decompress(raw, size, result);
-            } else {
-                int value_size = uFiles[top].read(raw);
-                while (value_size <= 0 && top < uFiles.length) {
-                    ++top;
-                    if (top < uFiles.length) {
-                        value_size = uFiles[top].read(raw);
+                    if (top >= uFiles.length) {
+                        return -1;
                     }
+                    int size = Macros.decodeInt(temp, 0);
+                    System.out.println(raw.length + " " + size);
+                    int cur = 0;
+                    do {
+                        cur += uFiles[top].read(raw, cur, size - cur);
+                    } while (cur < size);
+                    result_size = Macros.compressor.decompress(raw, size, result);
+                } else {
+                    int value_size = uFiles[top].read(raw);
+                    while (value_size <= 0 && top < uFiles.length) {
+                        ++top;
+                        if (top < uFiles.length) {
+                            value_size = uFiles[top].read(raw);
+                        }
+                    }
+                    if (top >= uFiles.length) {
+                        return -1;
+                    }
+                    result = raw;
+                    result_size = value_size;
                 }
-                if (top >= uFiles.length) {
-                    return -1;
-                }
-                result = raw;
-                result_size = value_size;
+                int first_length = Math.min(result_size, buffer.length - (now + size) % buffer.length);
+                System.arraycopy(result, 0, buffer, (now + size) % buffer.length, first_length);
+                System.arraycopy(result, first_length, buffer, 0, result_size - first_length);
+                size += result_size;
             }
-            int first_length = Math.min(result_size, buffer.length - (now + size) % buffer.length);
-            System.arraycopy(result, 0, buffer, (now + size) % buffer.length, first_length);
-            System.arraycopy(result, first_length, buffer, 0, result_size - first_length);
-            size += result_size;
         }
         System.arraycopy(buffer, now, dst, off, Math.min(len, buffer.length - now));
         System.arraycopy(buffer, 0, dst, off + Math.min(len, buffer.length - now), len - Math.min(len, buffer.length - now));
         now = (now + len) % buffer.length;
         size -= len;
         return len;
+
     }
 }
